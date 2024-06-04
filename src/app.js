bd653311b6fad179fe08fc02559effc380fd6431
Sam Sauer <sam.sauer@krekeny.com>
2024-06-05 00:56:43 +0200

    fix(app): use lodash memo during filer to reduce render lag

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import {
   Box,
   Button,
@@ -11,7 +11,9 @@ import {
   Th,
   Thead,
   Tr,
+  VStack,
 } from '@chakra-ui/react';
+import { debounce } from 'lodash';
 import './App.css';
 
 function App() {
@@ -24,20 +26,25 @@ function App() {
       .then(data => setData(data));
   }, []);
 
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value.toLowerCase());
-  };
+  const handleFilterChange = useCallback(
+    debounce((event) => {
+      setFilter(event.target.value.toLowerCase());
+    }, 300),
+    []
+  );
 
-  const filteredData = data.filter(item => {
-    return (
-      item.url.toLowerCase().includes(filter) ||
-      item.headline.toLowerCase().includes(filter) ||
-      (item.meeting_info.meeting_type || '').toLowerCase().includes(filter) ||
-      (item.meeting_info.date || '').toLowerCase().includes(filter) ||
-      (item.meeting_info.session || '').toLowerCase().includes(filter) ||
-      (item.meeting_info.decision || '').toLowerCase().includes(filter)
-    );
-  });
+  const filteredData = useMemo(() => {
+    return data.filter(item => {
+      return (
+        item.url.toLowerCase().includes(filter) ||
+        item.headline.toLowerCase().includes(filter) ||
+        (item.meeting_info.meeting_type || '').toLowerCase().includes(filter) ||
+        (item.meeting_info.date || '').toLowerCase().includes(filter) ||
+        (item.meeting_info.session || '').toLowerCase().includes(filter) ||
+        (item.meeting_info.decision || '').toLowerCase().includes(filter)
+      );
+    });
+  }, [data, filter]);
 
   const showArticle = (articleHtml) => {
     const articleWindow = window.open('', '_blank');
@@ -48,14 +55,18 @@ function App() {
   return (
     <Container maxW="container.xl" py={4}>
       <Heading as="h1" mb={4}>Verfügbare Beschlüsse</Heading>
-      <Input
-        type="text"
-        id="filter"
-        placeholder="Filter"
-        value={filter}
-        onChange={handleFilterChange}
-        mb={4}
-      />
+      <VStack spacing={4} align="stretch" mb={4}>
+        <Box>
+          <Input
+            type="text"
+            id="filter"
+            placeholder="Suchen"
+            onChange={handleFilterChange}
+            fontSize="2xl"
+            p={4}
+          />
+        </Box>
+      </VStack>
       <Box overflowX="auto">
         <Table variant="striped" colorScheme="teal">
           <Thead>
